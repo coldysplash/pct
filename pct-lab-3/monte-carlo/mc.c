7f2ef51ae2db7691ad0cf586c97c0c9cd0a6e8c5
ReshetnikovIvan121 <ivanreshetnikov235@gmail.com>
2023-05-17 16:47:46 +0700

    formating

--- pct-lab-3/monte-carlo/mc.c
+++ pct-lab-3/monte-carlo/mc.c
@@ -6,45 +6,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double
-func (double x, double y)
-{
-  return x / (y * y);
-}
+double func(double x, double y) { return x / (y * y); }
 
-double
-getrand (unsigned int *seed)
-{
-  return (double)rand_r (seed) / RAND_MAX;
-}
+double getrand(unsigned int *seed) { return (double)rand_r(seed) / RAND_MAX; }
 
-int
-main (int argc, char **argv)
-{
-  if (argc < 2)
-    return 1;
-  int num_thds = atoi (argv[1]);
-  double t = omp_get_wtime ();
+int main(int argc, char **argv) {
+  if (argc < 2) return 1;
+  int num_thds = atoi(argv[1]);
+  double t = omp_get_wtime();
   const int n = 10000000;
-  printf ("Numerical integration by Monte Carlo method: n = %d\n", n);
+  printf("Numerical integration by Monte Carlo method: n = %d\n", n);
   int in = 0;
   double s = 0;
 #pragma omp parallel num_threads(num_thds)
   {
     double s_loc = 0;
     int in_loc = 0;
-    unsigned int seed = omp_get_thread_num ();
+    unsigned int seed = omp_get_thread_num();
 #pragma omp for nowait
-    for (int i = 0; i < n; i++)
-      {
-        double x = getrand (&seed);         /* x in [0, 1] */
-        double y = 2 + getrand (&seed) * 3; /* y in [2, 5] */
-        if (y <= 5 && y >= 2 && x >= 0 && x <= 1)
-          {
-            ++in_loc;
-            s_loc += func (x, y);
-          }
+    for (int i = 0; i < n; i++) {
+      double x = getrand(&seed);         /* x in [0, 1] */
+      double y = 2 + getrand(&seed) * 3; /* y in [2, 5] */
+      if (y <= 5 && y >= 2 && x >= 0 && x <= 1) {
+        ++in_loc;
+        s_loc += func(x, y);
       }
+    }
 #pragma omp atomic
     s += s_loc;
 #pragma omp atomic
@@ -52,8 +39,8 @@ main (int argc, char **argv)
   }
   double v = 3 * in / n;
   double res = v * s / in;
-  printf ("Result: %.12f, n %d\n", res, n);
-  t = omp_get_wtime () - t;
-  printf ("Elapsed time (sec.): %.6f\n", t);
+  printf("Result: %.12f, n %d\n", res, n);
+  t = omp_get_wtime() - t;
+  printf("Elapsed time (sec.): %.6f\n", t);
   return 0;
 }
--- pct-lab-5/generator.c
+++ pct-lab-5/generator.c
@@ -2,32 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-void gen(int n)
-{
-    static int fn = 1;
-    char bf[32];
-    sprintf(bf, "data/%d.txt", fn);
-    FILE* f = fopen(bf, "w");
-    if (!f) {
-        fprintf(stderr, "gen: fatal error: f == NULL\n");
-        return;
-    }
-    while (n-- > 0) {
-        fprintf(f, "%d\n", rand());
-    }
-    ++fn;
+void gen(int n) {
+  static int fn = 1;
+  char bf[32];
+  sprintf(bf, "data/%d.txt", fn);
+  FILE* f = fopen(bf, "w");
+  if (!f) {
+    fprintf(stderr, "gen: fatal error: f == NULL\n");
+    return;
+  }
+  while (n-- > 0) {
+    fprintf(f, "%d\n", rand());
+  }
+  ++fn;
 }
 
-int main(int argc, char** argv)
-{
-    if (argc < 4)
-        return 1;
-    srand(time(0));
-    int start = atoi(argv[1]);
-    int end = atoi(argv[2]);
-    int step = atoi(argv[3]);
-    for (; start <= end; start += step) {
-        gen(start);
-    }
-    return 0;
+int main(int argc, char** argv) {
+  if (argc < 4) return 1;
+  srand(time(0));
+  int start = atoi(argv[1]);
+  int end = atoi(argv[2]);
+  int step = atoi(argv[3]);
+  for (; start <= end; start += step) {
+    gen(start);
+  }
+  return 0;
 }
